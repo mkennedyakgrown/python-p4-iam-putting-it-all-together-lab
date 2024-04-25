ee378e7cf0d7cad72555389069cb96d93d8e08f7
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-04-24 19:17:15 -0500

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -8,26 +8,110 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    
+    def post(self):
+        json = request.get_json()
+        user = User()
+        try:
+            user.username = json.get('username', '')
+            user.password_hash = json.get('password')
+            user.bio = json.get('bio')
+            user.image_url = json.get('image_url')
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return {
+                'user_id': session['user_id'],
+                'username': user.username,
+                'image_url': user.image_url,
+                'bio': user.bio
+            }, 201
+        except:
+            return {'message': 'Error creating user'}, 422
+    
+
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user_to_dict(user), 200
+        else:
+            return {'message': 'User not logged in'}, 401
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+        password = request.get_json()['password']
+        if user is not None and user.authenticate(password) == True:
+            session['user_id'] = user.id
+            return user_to_dict(user), 200
+        else:
+            return {'message': 'Username and password do not match any users'}, 401
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        if session['user_id']:
+            session['user_id'] = None
+            return {'message': 'Successfully Logged Out'}, 204
+        else:
+            return {'message': 'Cannot logout: not logged in'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    
+    def get(self):
+        if session['user_id']:
+            recipes = [{
+                'title': recipe.title,
+                'instructions': recipe.instructions,
+                'minutes_to_complete': recipe.minutes_to_complete,
+                'user': user_to_dict(User.query.filter_by(id=recipe.user_id).first())
+            } for recipe in Recipe.query.all()]
+            return recipes, 200
+        else:
+            return {'message': 'Must be logged in to view'}, 401
+        
+    def post(self):
+        if session['user_id']:
+            json = request.get_json()
+            recipe = Recipe()
+            try:
+                recipe.title = json.get('title')
+                recipe.instructions = json.get('instructions')
+                recipe.minutes_to_complete = json.get('minutes_to_complete')
+                recipe.user_id = session['user_id']
+                db.session.add(recipe)
+                db.session.commit()
+                recipe_json = {
+                    'title': recipe.title,
+                    'instructions': recipe.instructions,
+                    'minutes_to_complete': recipe.minutes_to_complete,
+                    'user': user_to_dict(User.query.filter_by(id=recipe.user_id).first())
+                }
+                return recipe_json, 201
+            except:
+                return {'message': 'Recipe could not be created'}, 422
+        else:
+            return {'message': 'Must be logged in to create a recipe'}, 401
+        
+def user_to_dict(user):
+    return {
+        'id': user.id,
+        'username': user.username,
+        'image_url': user.image_url,
+        'bio': user.bio
+    }
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,73 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship(
+        'Recipe', back_populates='user', cascade='all, delete-orphan')
+
+    @validates('username')
+    def validate_username(self, key, name):
+        if name == '':
+            raise ValueError('Username cannot be an empty string')
+        elif self.query.filter_by(username=name).first() is not None:
+            raise ValueError('This name already exists')
+        else:
+            return name
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}'
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, db.CheckConstraint('LENGTH(instructions) >= 50)'), nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship(
+    'User', back_populates='recipes', uselist=False)
+
+    __table_args__ = (db.CheckConstraint('LENGTH(instructions) >= 50'),)
+
+    @validates('title')
+    def validate_title(self, key, title_text):
+        if title_text == '':
+            raise ValueError('Title cannot be an empty string')
+        else:
+            return title_text
+        
+    @validates('instructions')
+    def validate_instructions(self, key, inst):
+        if inst == '':
+            raise ValueError('Instructions cannot be an empty string')
+        # elif len(inst) < 50:
+        #     raise ValueError('Instructions must be at least 50 characters long')
+        else:
+            return inst
